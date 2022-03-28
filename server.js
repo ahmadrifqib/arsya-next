d905e57f169a23f43ca2cb0a0e506e9fdb7edf20
ahmadrifqib <ahmad.rifqi@outlook.co.id>
2022-03-28 11:11:33 +0700

    initial build 3

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-// const hostname = "localhost";
-const port = process.env.port || 3000;
+const hostname = "localhost";
+const port = process.env.PORT || 3000;
 // when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, port });
+const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -31,6 +31,6 @@ app.prepare().then(() => {
 		}
 	}).listen(port, (err) => {
 		if (err) throw err;
-		console.log("> Ready on http://${hostname}:${port}");
+		console.log(`> Ready on http://${hostname}:${port}`);
 	});
 });
